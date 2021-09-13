1ae045c9c85bad3c0ed2114309a8050bc1b0a55a
Casey Occhialini <1508707+littlespex@users.noreply.github.com>
2021-09-13 13:38:44 -0700

    release/0.3.1 (#12)
    * Move tests to separate folder
    * Make shards a standalone object
    * Add project bundling and minification

--- src/CmcdShards.ts
+++ src/CmcdShards.ts
@@ -1,6 +1,6 @@
-export enum CmcdShards {
-  REQUEST = 'request',
-  OBJECT = 'object',
-  SESSION = 'session',
-  STATUS = 'status',
-}
+export const CmcdShards = {
+  object: ['br', 'd', 'ot', 'tb'],
+  request: ['dl', 'mtp', 'nor', 'nrr', 'su'],
+  session: ['cid', 'pr', 'sf', 'sid', 'st', 'v'],
+  status: ['bs', 'rtp'],
+};
