04dfe88e5456c4f46f2dd64688db653e50cc7ce8
Casey Occhialini <1508707+littlespex@users.noreply.github.com>
2021-09-13 13:38:02 -0700

    no message

--- src/CmcdShards.ts
+++ src/CmcdShards.ts
@@ -1,6 +1,6 @@
 export const CmcdShards = {
-  request: ['dl', 'mtp', 'nor', 'nrr', 'su'],
   object: ['br', 'd', 'ot', 'tb'],
-  status: ['bs', 'rtp'],
+  request: ['dl', 'mtp', 'nor', 'nrr', 'su'],
   session: ['cid', 'pr', 'sf', 'sid', 'st', 'v'],
+  status: ['bs', 'rtp'],
 };
