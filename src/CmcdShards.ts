283f431c2edeb2fd3e235020791a92e3c49f539c
Casey Occhialini <1508707+littlespex@users.noreply.github.com>
2021-09-15 13:42:53 -0700

    - Add optional formatting - Make sorting optional - Add missing bl property

--- src/CmcdShards.ts
+++ src/CmcdShards.ts
@@ -1,6 +1,6 @@
 export const CmcdShards = {
   object: ['br', 'd', 'ot', 'tb'],
-  request: ['dl', 'mtp', 'nor', 'nrr', 'su'],
+  request: ['bl', 'dl', 'mtp', 'nor', 'nrr', 'su'],
   session: ['cid', 'pr', 'sf', 'sid', 'st', 'v'],
   status: ['bs', 'rtp'],
 };
